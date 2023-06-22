c1c42a85e130e89d1d5b7f1edf203b884f5c54c2
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-06-21 23:35:11 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+<!--
+    
+    Create the following elements:
+
+
+two number input fields
+
+5 buttons corresponding to the 5 basic arithmetic operations (addition, subtraction, multiplication, division)
+
+paragraph with span for result
+
+
+Use the necessary query selectors and attributes to perform the necessary operation between the two numbers when a corresponding button is pressed.
+
+-->
+
+
+
+
+
+let a = document.createElement("input");
+i.setAttribute("type", "number");
+
+let b = document.createElement("input");
+t.setAttribute("type", "number")
+
+let x = 10;
+let y = 6;
+let z = 5;
+
+let add = document.createElement("input")
+add.setAttribute("type", "button");
+
+let subtract = document.createElement("input");
+subtract.setAttribute("type", "button");
+
+let multiplication = document.createElement("input");
+multiplication = document.createAttribute("type", "button");
+
+let division = document.createElement("input");
+division = document.createAttribute("type", "button")
+
+let p = document.createElement("p");
+const textnode = document.createTextNode("paragraph");
+
+function appendFunction() {
+
+    let sp = ducument.createElement('span');
+    document.getElementById("paragraph").appendChild(p);
+}
